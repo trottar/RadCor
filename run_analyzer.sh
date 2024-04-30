ac4b460f01800f66aa69016ba05c57ff9370f283
Richard Trotta <trotta@cua.edu>
2024-04-30 08:00:52 -0400

    Testing RadCorr example

--- run_analyzer.sh
+++ run_analyzer.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-30 07:56:40 trottar"
+# Time-stamp: "2024-04-30 07:57:14 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -54,7 +54,6 @@ if [[ $c_flag == "true" ]]; then
     echo
     cd "CAnalyzer-master"
     make clean
-    make
     make
     cd "../"
 fi
