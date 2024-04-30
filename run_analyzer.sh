d7cbb7e876408d7300661d053130c50c252a26ab
Richard Trotta <trotta@cua.edu>
2024-04-30 07:28:37 -0400

    Testing RadCorr example

---
+++ run_analyzer.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+
+#
+# Description:
+# ================================================================
+# Time-stamp: "2024-04-30 07:28:03 trottar"
+# ================================================================
+#
+# Author:  Richard L. Trotta III <trottar.iii@gmail.com>
+#
+# Copyright (c) trottar
+#
+
+# Flag definitions (flags: h, c)
+while getopts 'hca' flag; do
+    case "${flag}" in
+        h) 
+        echo "--------------------------------------------------------------"
+        echo "./run_analyzer.sh -{flags} {variable arguments, see help}"
+	echo
+        echo "Description: "
+        echo "--------------------------------------------------------------"
+        echo
+        echo "The following flags can be called for the RadCorr analysis..."
+        echo "    -h, help"
+        echo "    -c, compile CAnalyzer"
+	echo "    -a, radiate function for all a1n d2n kinematics (11, 18, 30deg) and polarizations (unpol, long, trans)"
+	echo
+        exit 0
+        ;;
+	c) c_flag='true' ;;
+	r) a_flag='true' ;;
+        *) print_usage
+        exit 1 ;;
+    esac
+done
+
+dataset_config_filename="data_example"
+output_fiilename="radcor_out"
+
+# Source proper root version
+source /apps/root/6.22.06/setroot_CUE.bash
+
+# Compile CAnalyzer
+if [[ $c_flag == "true" ]]; then
+    cd "CAnalyzer-master"
+    make clean
+    make
+fi
+
+# Load ROOT macro
+cd "CAnalyzer-master/example/"
+if [[ $a_flag == "true" ]]; then
+    root -l <<EOF 
+    .L radiate_all()
+    EOF
+else
+    root -l <<EOF
+    .L rad_corr.C(“configs/${dataset_config_filename}.conf”,“output/${output_fiilename}.dat”)
+    .L radiate(“configs/${dataset_config_filename}.conf”,“output/${output_fiilename}.dat”)
+    EOF
+fi
