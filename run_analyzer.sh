cc047a44e90f6cf7a40132a5d4c4b0066a75fc45
Richard Trotta <trotta@cua.edu>
2024-04-30 09:10:25 -0400

    Testing RadCorr example

--- run_analyzer.sh
+++ run_analyzer.sh
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2024-04-30 09:09:29 trottar"
+# Time-stamp: "2024-04-30 09:10:22 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trottar.iii@gmail.com>
@@ -67,12 +67,12 @@ echo
 echo
 cd "CAnalyzer-master/example/"
 if [[ $a_flag == "true" ]]; then
-    root -l -q -L <<EOF 
+    root -l -b -L <<EOF 
 .L rad_corr.C
 radiate_all()
 EOF
 else
-    root -l -q -L <<EOF
+    root -l -b -L <<EOF
 .L rad_corr.C
 rad_corr("configs/${dataset_config_filename}.conf","output/${radcorr_output_fiilename}.dat")
 radiate("configs/${dataset_config_filename}.conf","output/${radiate_output_fiilename}.dat")
